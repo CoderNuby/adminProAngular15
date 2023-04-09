09d748ef49f34e6f365f18676f209ab8da9805ce
Luis <asdf32easd@gmail.com>
2023-04-08 17:19:14 -0700

    templates de paginas principales funcionando

---
+++ src/app/pages/grafica-first/grafica-first.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grafica-first',
+  templateUrl: './grafica-first.component.html',
+  styles: [
+  ]
+})
+export class GraficaFirstComponent {
+
+}
